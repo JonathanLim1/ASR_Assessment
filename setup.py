ebc6ff828dc9ad623f53a130c5bdb591275830ae
JonathanLim1 <JonathanLim1703@gmail.com>
2022-08-12 17:18:07 +0800

    Misc. for package upload

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Research',
+    'Operating System :: MacOS',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name = 'asr_phoneme_assessment',
+    version = '0.0.1',
+    description = 'Provides Phoneme Error Rate & Visualisation Assessment',
+    long_description = open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author = 'Jonathan Lim',
+    author_email = 'Jonathanlimws@gmail.com',
+    license = 'MIT',
+    classifiers = classifiers,
+    keywords = 'ASR',
+    packages = find_packages(),
+    install_requires = ['glob',
+                        'tqdm',
+                        'librosa',
+                        'scipy.io.wavfile',
+                        'numpy',
+                        'pandas',
+                        'sklearn',
+                        'pydub',
+                        'soundfile',
+                        'subprocess',
+                        'copy',
+                        'difflib',
+                        'eng_to_ipa']
+)
