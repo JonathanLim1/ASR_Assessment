df81664fac45baf1b9267224f06a4a18bd512458
JonathanLim1 <JonathanLim1703@gmail.com>
2022-08-12 17:36:59 +0800

    Misc for package upload on PyPI

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 classifiers = [
     'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Research',
+    'Intended Audience :: Science/Research',
     'Operating System :: MacOS',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3'
 ]
 
 setup(
-    name = 'asr_phoneme_assessment',
+    name = 'asrassessment',
     version = '0.0.1',
     description = 'Provides Phoneme Error Rate & Visualisation Assessment',
     long_description = open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
